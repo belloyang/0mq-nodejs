1b16795d67d9a9d49a056b9a86d4e3f268dc7adf
Bing Yang <bingyang@nanometrics.ca>
2021-04-16 20:05:47 -0400

    First commit

---
+++ src/client.js
@@ -0,0 +1,71 @@
+var zmq = require('zeromq');
+
+// socket to talk to server
+console.log("Connecting to hello world server...");
+var requester = zmq.socket('req');
+
+var x = 0;
+requester.on("message", function(reply) {
+  // console.log("Received reply", x, ": [", reply.toString(), ']');
+  
+  let replyJson = reply.toString();
+  try{
+    let replyObj = JSON.parse(replyJson);
+    console.log('***client receives replyObj ', replyObj);
+    let replyType = replyObj.type;
+    if(replyType === 'ExecID') {
+      console.log('receive msg', replyType);
+      let harvestCall = {
+      call: 'get_op_responses',
+      parameters: [replyObj.execId, 0],
+      }
+      requester.send(JSON.stringify(harvestCall));
+    } else {
+      console.log('receive msg', replyType, replyObj);
+      let subscriber = zmq.socket('sub');
+      subscriber.connect('tcp://localhost:5556');
+      console.log("Subscriber connected to port 5556");
+      subscriber.subscribe("get_op_responses");
+      subscriber.on("message", function(topic, message) {
+        console.log(
+          "received a reply for:",
+          topic.toString(),
+          ":",
+          message.toString()
+        );
+        let replyObj = JSON.parse(message.toString());
+        let replyType = replyObj.type;
+        console.log('receive subscribed msg', replyType, replyObj.response);
+        if(replyObj.response.type === 'completion') {
+            subscriber.unsubscribe('get_op_responses');
+            requester.close();
+            process.exit(0); 
+        }
+      });
+      
+      
+    }
+    
+  }catch(e) {
+    console.error('failed to parse replyJson', replyJson, e);
+  }
+  
+  
+  
+});
+
+requester.connect("tcp://localhost:5555");
+
+
+console.log("Sending request", '...');
+  let harvestCall = {
+    call: 'list',
+    parameters: [],
+    expect_response: true
+  }
+  requester.send(JSON.stringify(harvestCall));
+
+
+process.on('SIGINT', function() {
+  requester.close();
+});
---
+++ src/main.js
@@ -0,0 +1,97 @@
+
+// Binds REP socket to tcp://*:5555
+// Binds pub socket to tcp://*.5556
+var { HarvesterAPIs }=  require('@nanometrics/pegasus-harvest-lib');
+
+var zmq = require('zeromq');
+
+// socket to talk to clients
+var responder = zmq.socket('rep');
+
+responder.on('message', function(request) {
+  console.log("Received request: [", request.toString(), "]");
+
+   let msgJson = request.toString();
+   try{
+     let msgObject = JSON.parse(msgJson);
+    console.log('receive JSON:', msgObject);
+
+    switch(msgObject.call) {
+      case  'harvest_data': {
+        // call harvest_data
+        let execId =  HarvesterAPIs.harvest_data(...msgObject['parameters']);
+        let replyObj = {
+          type: 'ExecID',
+          execId: execId
+        }
+        responder.send(JSON.stringify(replyObj));
+      }break;
+      case 'list': {
+        let execId = HarvesterAPIs.list();
+        let replyObj = {
+          type: 'ExecID',
+          execId: execId
+        }
+        console.log('server: send reply from call: list', replyObj);
+        responder.send(JSON.stringify(replyObj));
+      }break;
+      case 'get_op_responses': {
+        let replyObj = {
+          type: 'Response',
+          response: null
+        }
+        
+        let publisher = zmq.socket('pub');
+        publisher.bindSync('tcp://*:5556');
+        console.log("Publisher bound to port 5556");
+        responder.send(JSON.stringify(replyObj));
+        let getOPResponsePollHandle = setInterval(() => {
+        let queryStr = HarvesterAPIs.get_op_responses(...msgObject['parameters']);
+          if(queryStr !== null) {
+            let query = JSON.parse(queryStr);
+            // console.log('check query:', query);
+            let responseJSON = query.responses;
+            for (let responseStr of responseJSON) {
+              let replyObj = {
+              type: 'Response',
+              response: JSON.parse(responseStr)
+              }
+              
+                console.log('server: SEND reply from call: get_op_responses', replyObj);
+                
+                publisher.send(['get_op_responses', JSON.stringify(replyObj)]);
+
+            }
+          } else {
+            console.log('clearInterval: receive null from get_op_responses');
+            publisher.close();
+            clearInterval(getOPResponsePollHandle);
+          }
+        }, 100);
+        
+      }break;
+      default:
+        let replyObj = {
+          unsupported_call: execId
+        }
+        responder.send(JSON.stringify(replyObj));
+
+    }
+
+   }catch(e) {
+     console.error('failed to pass json string', msgJson, e);
+   }
+   
+});
+
+responder.bind('tcp://*:5555', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Listening on 5555...");
+  }
+});
+
+process.on('SIGINT', function() {
+  responder.close();
+});
