0a247ecb2569ff5103e9160d2b948428bbcce11d
Bing Yang <bingyang@nanometrics.ca>
2021-04-21 14:07:45 -0400

    call list and harvest_data from new-client

--- src/new-client.ts
+++ src/new-client.ts
@@ -1,6 +1,8 @@
+import { UpperLimit } from '@nanometrics/pegasus-harvest-lib';
 import { Subject } from 'rxjs';
 
 import {ExecHarvestCmd} from './client-interface';
+import { HarvesterAPIParams_HarvestData } from './models/api-params';
 
 console.log('calling ExecHarvestCmd from new client');
 
@@ -10,5 +12,33 @@ ExecHarvestCmd('list', {}, (execId, response) => {
     responseSubject = response;
     responseSubject.subscribe((response) => {
         console.log('***new-client:receive response for list', response);
+        if(response.type == 'response') {
+            let system_path = response.data.system_path;
+            let params: HarvesterAPIParams_HarvestData = {
+                libPath: system_path,
+                params: {
+                  range: {
+                      lower: {
+                        time_microsecs: 0
+                      },
+                      upper: {
+                        time_microsecs: UpperLimit.timestamp
+                      },
+                  },
+                  output_path: './output',
+                  output_pattern: '${N}/${S}/${S}.${N}.${J}',
+                  hours_per_file: 24,
+                },
+                updateStep: 0.1 //report every 10% progress
+              }
+              console.log('***new-client: call harvest_data', params);
+              ExecHarvestCmd('harvest_data', params, (execId, response) => {
+                console.log(`***new-client: harvest_data return`, execId);
+                response.subscribe((resp)=> {
+                    console.log('***new-client:receive response for harvest_data', resp);
+                })
+              });
+              
+        }
     })
 });
